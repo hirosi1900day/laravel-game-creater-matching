053f730de37e156ebe4934691b002aad4a68b53f
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-06-27 16:39:12 +0900

    投稿ホーム作成

--- backend/resources/js/pages/Postform.vue
+++ backend/resources/js/pages/Postform.vue
@@ -1,11 +1,78 @@
 <template>
   <div class="photo-form">
     <h2 class="title">投稿</h2>
-    <form class="form">
-      <input class="form__item" type="text">
+    <form class="form" @submit.prevent="submit">
+      <input class="form__item" type="text" v-model="postData.title">
+      <input class="form__item" type="text" v-model="postData.content"> 
+      <div v-if="errors" class="errors">
+        <ul v-if="errors.title">
+          <li v-for="msg in errors.title" :key="msg">{{ msg }}</li>
+        </ul>
+        <ul v-if="errors.content">
+          <li v-for="msg in errors.content" :key="msg">{{ msg }}</li>
+        </ul>
+      </div>
       <div class="form__button">
-        <button type="submit" class="button button--inverse">submit</button>
+        <button>submit</button>
       </div>
     </form>
   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import { UNPROCESSABLE_ENTITY } from '../util'
+
+export default {
+  data () {
+    return { 
+      postData: {
+        title: "",
+        content: "",
+      },
+      validationCheck: false
+    }
+  },
+  computed: {
+    ...mapState({
+      errors: state => 
+      { 
+        console.log(state.error.errorMessages)
+        return state.error.errorMessages
+      }
+    })
+  },
+  methods: {
+    checkForm: function (e) {
+      if (this.postData.title && this.postData.content) {
+        return true;
+      }
+      this.errors = [];
+
+      if (this.postData.title == "") {
+        this.errors.push('タイトルが必要です');
+      }
+      if (this.postData.content == "") {
+        this.errors.push('内容を入力してください');
+      }
+      e.preventDefault();
+    },
+    reset () {
+      this.postData.title = ""
+      this.postData.content = ""
+      console.log("リセット")
+    },
+    async submit () {
+      console.log("確認")
+      const response = await axios.post('/api/post', this.postData)
+      if (response.status === UNPROCESSABLE_ENTITY) {
+        this.$store.commit('error/setErrorMessages', response.data.errors)
+      } else {
+        this.$store.commit('error/setCode', response.status)
+      }
+      this.reset()
+      console.log("確認")
+    }
+  }
+}
+</script>
